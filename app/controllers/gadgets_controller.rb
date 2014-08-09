18af8e4c0111cbe5a3d8bde9d5bd5a28e4cd33bd
Andrea Mostosi <andrea.mostosi@zenkay.net>
2014-08-09 08:28:26 +0200

    Added FactoryGirl and Devise

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -1,4 +1,5 @@
 class GadgetsController < ApplicationController
+  before_action :authenticate_user!
   def index
     
   end
--- spec/controllers/gadgets_controller_spec.rb
+++ spec/controllers/gadgets_controller_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe GadgetsController, :type => :controller do
+
+  before(:each) do
+    @user_attr = FactoryGirl.attributes_for(:user)
+    @user = User.create!(@user_attr)
+  end
+
   describe "GET #index" do
     it "responds successfully with an HTTP 200 status code" do
+      sign_in @user
       get :index
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
   end
+
+
 end
